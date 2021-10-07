21f124744103ca1ef1d716ec895f0bf95f46b05e
XMnikita <Xmnikita.01@mail.ru>
2021-10-07 13:23:18 +0300

    Доработал форму

--- UchetBibl/Form1.Designer.cs
+++ UchetBibl/Form1.Designer.cs
@@ -37,6 +37,8 @@ namespace UchetBibl
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -61,7 +63,7 @@ namespace UchetBibl
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 231);
+            this.listBox1.Location = new System.Drawing.Point(12, 121);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(366, 303);
             this.listBox1.TabIndex = 2;
@@ -69,7 +71,7 @@ namespace UchetBibl
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(422, 231);
+            this.listBox2.Location = new System.Drawing.Point(422, 121);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(366, 303);
             this.listBox2.TabIndex = 3;
@@ -77,18 +79,19 @@ namespace UchetBibl
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(12, 540);
+            this.button2.Location = new System.Drawing.Point(12, 518);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(776, 38);
             this.button2.TabIndex = 4;
             this.button2.Text = "Добавить задолжность";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 202);
+            this.label1.Location = new System.Drawing.Point(12, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(97, 26);
             this.label1.TabIndex = 5;
@@ -98,7 +101,7 @@ namespace UchetBibl
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(417, 202);
+            this.label2.Location = new System.Drawing.Point(417, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 26);
             this.label2.TabIndex = 6;
@@ -114,11 +117,33 @@ namespace UchetBibl
             this.label3.TabIndex = 7;
             this.label3.Text = "Введите фамилию чтобы посмотреть задолжности";
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(776, 38);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Обновить списки";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(12, 474);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(776, 38);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Удалить задолжность";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -144,6 +169,8 @@ namespace UchetBibl
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
